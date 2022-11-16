4b4203c28eafdac896a1d57c9dada0c862da51da
Klaven Jones <klavensjones@gmail.com>
2022-11-16 13:16:59 -0500

    chore: add linting rules

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello 8th Lighters!"', () => {
+      expect(appController.getHello()).toBe('Hello 8th Lighters!');
     });
   });
 });
