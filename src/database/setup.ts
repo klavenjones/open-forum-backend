724b50bb9e7533a5d046ec4a9f38f105bce41db5
Klaven Jones <klavensjones@gmail.com>
2022-11-29 19:05:30 -0500

    Add db role to database

--- src/database/setup.ts
+++ src/database/setup.ts
@@ -13,24 +13,30 @@ const pgclient = new Client({
 
 pgclient.connect();
 
+const dbQuery = (query: string, successMessage: string, exitScript = false) => {
+  pgclient.query(query, error => {
+    if (error) {
+      console.log('ERROR', error);
+      process.exit(1);
+    }
+    console.log(successMessage);
+    if (exitScript) process.exit(0);
+  });
+};
+
+//Creating a role for the user.
+const createRole = `CREATE ROLE ${process.env.DATABASE_USER} LOGIN CREATEDB`;
+dbQuery(createRole, `CREATED ROLE NAMED ${process.env.DATABASE_USER} SUCCESFULLY`);
+
+//Add password to role if provided
+if (process.env.DATABASE_PASSWORD) {
+  const addPassword = `ALTER ROLE ${process.env.DATABASE_USER} with PASSWORD ${process.env.DATABASE_PASSWORD}`;
+  dbQuery(addPassword, 'PASSWORD ADDED TO ROLE SUCCESSFULLY');
+}
+
+//Create Development Databases
 const dropDatabaseIfExists = `DROP DATABASE IF EXISTS ${process.env.DATABASE_NAME}`;
+dbQuery(dropDatabaseIfExists, `DROPPED DATABASE SUCCESFULLY`);
+
 const createDatabase = `CREATE DATABASE ${process.env.DATABASE_NAME}`;
-
-pgclient.query(dropDatabaseIfExists, error => {
-  if (error) {
-    console.log('ERROR', error);
-    process.exit(1);
-  }
-
-  console.log(`Database dropped succesfully`);
-});
-
-pgclient.query(createDatabase, error => {
-  if (error) {
-    console.log('ERROR', error);
-    process.exit(1);
-  }
-
-  console.log(`Database created succesfully`);
-  process.exit(0);
-});
+dbQuery(createDatabase, `CREATED DATABASE SUCCESFULLY`, true);
