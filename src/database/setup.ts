bc8e48b15cf0b652352d97af496c9121592f0475
Klaven Jones <klavensjones@gmail.com>
2022-12-07 14:21:38 -0500

    Add user to setup.ts

--- src/database/setup.ts
+++ src/database/setup.ts
@@ -7,6 +7,7 @@ dotenv.config();
 const pgclient = new Client({
   host: process.env.DATABASE_HOST,
   port: 5432,
+  user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: 'postgres',
 });
