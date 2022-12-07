125ae102215ff6b42b56e217dcac2da19097353f
Klaven Jones <klavensjones@gmail.com>
2022-12-07 13:44:02 -0500

    Add registration e2e tests and imported register.module to the api.module

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { RegisterModule } from './auth/register/register.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, RegisterModule],
 })
 export class ApiModule {}
---
+++ test/register.e2e-spec.ts
@@ -0,0 +1,99 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import * as request from 'supertest';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RegisterUserDto } from '../src/api/auth/register/dto/register-user.dto';
+import * as dotenv from 'dotenv';
+import { RegisterModule } from '../src/api/auth/register/register.module';
+import { UserModule } from '../src/api/user/user.module';
+
+dotenv.config();
+
+describe('Register user API Testing (e2e)', () => {
+  let app: INestApplication;
+  const registerUser: RegisterUserDto = { username: 'Klaven', password: 'tester' };
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [
+        UserModule,
+        RegisterModule,
+        TypeOrmModule.forRootAsync({
+          useFactory: () => ({
+            type: 'postgres',
+            host: process.env.TEST_DATABASE_HOST,
+            username: process.env.TEST_DATABASE_USER,
+            port: 5432,
+            database: process.env.TEST_DATABASE_NAME,
+            password: process.env.TEST_DATABASE_PASSWORD,
+            synchronize: true,
+            autoLoadEntities: true,
+            dropSchema: true,
+          }),
+        }),
+      ],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+    await app.init();
+  });
+
+  describe('Create a new user [POST /auth/register]', () => {
+    it('should return a Bad Request 400 error and array of error messages when a user forgets to send username in the request', () => {
+      const badRequest = { password: 'tester' };
+      return request(app.getHttpServer())
+        .post('/auth/register')
+        .send(badRequest)
+        .expect(400)
+        .then(({ body }) => {
+          expect(body.message.includes('username must be shorter than or equal to 120 characters')).toBe(true);
+          expect(body.message.includes('username should not be empty')).toBe(true);
+          expect(body.message.includes('username must be a string')).toBe(true);
+        });
+    });
+
+    it('should return a Bad Request 400 error and array of error messages when a user forgets to send username in the request when a user forgets to send password in the request', () => {
+      const badRequest = { username: 'klaven' };
+      return request(app.getHttpServer())
+        .post('/auth/register')
+        .send(badRequest)
+        .expect(400)
+        .then(({ body }) => {
+          expect(body.message.includes('password must be shorter than or equal to 60 characters')).toBe(true);
+          expect(body.message.includes('password should not be empty')).toBe(true);
+          expect(body.message.includes('password must be a string')).toBe(true);
+        });
+    });
+
+    it('should return a Bad Request 400 error and array of error messages when a user forgets to send username in the request when a user sends an empty object', () => {
+      return request(app.getHttpServer())
+        .post('/auth/register')
+        .send({})
+        .expect(400)
+        .then(({ body }) => {
+          expect(body.message.includes('username must be shorter than or equal to 120 characters')).toBe(true);
+          expect(body.message.includes('username should not be empty')).toBe(true);
+          expect(body.message.includes('username must be a string')).toBe(true);
+          expect(body.message.includes('password must be shorter than or equal to 60 characters')).toBe(true);
+          expect(body.message.includes('password should not be empty')).toBe(true);
+          expect(body.message.includes('password must be a string')).toBe(true);
+        });
+    });
+
+    it('should create a new user and send a success message', () => {
+      return request(app.getHttpServer())
+        .post('/auth/register')
+        .send(registerUser)
+        .expect(201)
+        .then(({ body }) => {
+          expect(body.message).toEqual('Hey Klaven your registration was successful!');
+          expect(body.status).toEqual(201);
+        });
+    });
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+});
