cbd954e83901e84f366d6ac637f8ede6941397e4
Klaven Jones <klavensjones@gmail.com>
2022-12-13 17:36:06 -0500

    Add JWT Secret and Cookie Parser

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,18 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors({
+    credentials: true,
+    origin: '*',
+  });
   const config: ConfigService = app.get(ConfigService);
   const port: number = config.get<number>('PORT');
 
+  app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
   await app.listen(port, () => {
