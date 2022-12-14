72ba1f81ba37a559139d54ea83faaea187950059
Klaven Jones <klavensjones@gmail.com>
2022-12-14 16:14:34 -0500

    Add env variable CORS ORIGIN URL to cors config

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,15 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   app.enableCors({
     credentials: true,
-    origin: '*',
+    origin: process.env.CORS_ORIGINS,
   });
   const config: ConfigService = app.get(ConfigService);
   const port: number = config.get<number>('PORT');
